03348b4882084492c4006a52c360877926e80a1d
MajinCry <cm@cmrms.com>
2016-09-02 14:11:10 +0100

    Initial stuff

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGetDataDir = new System.Windows.Forms.Button();
+            this.textboxGetDataDir = new System.Windows.Forms.TextBox();
+            this.groupboxGetDataDir = new System.Windows.Forms.GroupBox();
+            this.buttonRedate = new System.Windows.Forms.Button();
+            this.dialogGetDataDir = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupboxGetDataDir.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonGetDataDir
+            // 
+            this.buttonGetDataDir.Location = new System.Drawing.Point(13, 29);
+            this.buttonGetDataDir.Name = "buttonGetDataDir";
+            this.buttonGetDataDir.Size = new System.Drawing.Size(75, 23);
+            this.buttonGetDataDir.TabIndex = 0;
+            this.buttonGetDataDir.Text = "Open...";
+            this.buttonGetDataDir.UseVisualStyleBackColor = true;
+            this.buttonGetDataDir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textboxGetDataDir
+            // 
+            this.textboxGetDataDir.Location = new System.Drawing.Point(101, 31);
+            this.textboxGetDataDir.Name = "textboxGetDataDir";
+            this.textboxGetDataDir.Size = new System.Drawing.Size(375, 20);
+            this.textboxGetDataDir.TabIndex = 1;
+            // 
+            // groupboxGetDataDir
+            // 
+            this.groupboxGetDataDir.Controls.Add(this.textboxGetDataDir);
+            this.groupboxGetDataDir.Controls.Add(this.buttonGetDataDir);
+            this.groupboxGetDataDir.Location = new System.Drawing.Point(12, 12);
+            this.groupboxGetDataDir.Name = "groupboxGetDataDir";
+            this.groupboxGetDataDir.Size = new System.Drawing.Size(494, 74);
+            this.groupboxGetDataDir.TabIndex = 2;
+            this.groupboxGetDataDir.TabStop = false;
+            this.groupboxGetDataDir.Text = "Get Data Directory";
+            this.groupboxGetDataDir.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // buttonRedate
+            // 
+            this.buttonRedate.Location = new System.Drawing.Point(431, 92);
+            this.buttonRedate.Name = "buttonRedate";
+            this.buttonRedate.Size = new System.Drawing.Size(75, 23);
+            this.buttonRedate.TabIndex = 2;
+            this.buttonRedate.Text = "Redate!";
+            this.buttonRedate.UseVisualStyleBackColor = true;
+            this.buttonRedate.Click += new System.EventHandler(this.buttonRedate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 124);
+            this.Controls.Add(this.buttonRedate);
+            this.Controls.Add(this.groupboxGetDataDir);
+            this.Name = "Form1";
+            this.Text = "Fallout 4 - Bethesda Masters Redate";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupboxGetDataDir.ResumeLayout(false);
+            this.groupboxGetDataDir.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGetDataDir;
+        private System.Windows.Forms.TextBox textboxGetDataDir;
+        private System.Windows.Forms.GroupBox groupboxGetDataDir;
+        private System.Windows.Forms.Button buttonRedate;
+        private System.Windows.Forms.FolderBrowserDialog dialogGetDataDir;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (dialogGetDataDir.ShowDialog() == DialogResult.OK)
+            {
+                textboxGetDataDir.Text = dialogGetDataDir.SelectedPath;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        public bool CheckIfArrayIsNull(string strDataDir, out bool[] outputBoolArray)
+        {
+
+            bool[] boolarrayBethesdaMasters;
+            CheckForBethesdaMasters(strDataDir, out boolarrayBethesdaMasters);
+
+            int iNumberOfBooleans = boolarrayBethesdaMasters.Length;
+
+            bool boolValidMaster = false; // Default value. Don't want to set it to true just yet.
+
+            for (int iCounter = 0; iCounter < iNumberOfBooleans; iCounter++)
+            {
+                if (boolarrayBethesdaMasters[iCounter]
+                    && boolValidMaster == false)
+                {
+                    boolValidMaster = true;
+                }
+            }
+
+            outputBoolArray = boolarrayBethesdaMasters;
+            return boolValidMaster;
+
+        }
+
+        public void CheckForBethesdaMasters(string strDataDir, out bool[] outputBoolArray)
+        {
+            outputBoolArray = new bool[7];
+
+            if (File.Exists(strDataDir + "\\Fallout4.esm"))
+            {
+                outputBoolArray[0] = true;
+            }
+
+            else
+            {
+                outputBoolArray[0] = false;
+            }
+
+
+            if (File.Exists(strDataDir + "\\DLCRobot.esm"))
+            {
+                outputBoolArray[1] = true;
+            }
+
+            else
+            {
+                outputBoolArray[1] = false;
+            }
+
+
+            if (File.Exists(strDataDir + "\\DLCworkshop01.esm"))
+            {
+                outputBoolArray[2] = true;
+            }
+
+            else
+            {
+                outputBoolArray[2] = false;
+            }
+
+
+            if (File.Exists(strDataDir + "\\DLCCoast.esm"))
+            {
+                outputBoolArray[3] = true;
+            }
+
+            else
+            {
+                outputBoolArray[3] = false;
+            }
+
+
+            if (File.Exists(strDataDir + "\\DLCworkshop02.esm"))
+            {
+                outputBoolArray[4] = true;
+            }
+
+            else
+            {
+                outputBoolArray[4] = false;
+            }
+
+
+            if (File.Exists(strDataDir + "\\DLCworkshop03.esm"))
+            {
+                outputBoolArray[5] = true;
+            }
+
+            else
+            {
+                outputBoolArray[5] = false;
+            }
+
+
+            if (File.Exists(strDataDir + "\\DLCNukaWorld.esm"))
+            {
+                outputBoolArray[6] = true;
+            }
+
+            else
+            {
+                outputBoolArray[6] = false;
+            }
+
+        }
+
+        public void RedateMasters(string strDataDir, bool[] boolarrayBethesdaMasters)
+        {
+            
+            //Using the official release dates 'ere. Aren't I just fabulous?
+
+            DateTime datetimecreatedFallout4 = new DateTime(2015, 09, 10);
+
+            DateTime datetimecreatedAutomatron = new DateTime(2016, 03, 22);
+            DateTime datetimecreatedWastelandWorkshop = new DateTime(2016, 04, 12);
+            DateTime datetimecreatedFarHarbor = new DateTime(2016, 05, 19);
+            DateTime datetimecreatedContraptionsWorkshop = new DateTime(2016, 06, 21);
+            DateTime datetimecreatedVaultTecWorkshop = new DateTime(2016, 07, 26);
+
+            DateTime datetimecreatedNukaWorld = new DateTime(2016, 08, 30);
+
+            if (boolarrayBethesdaMasters[0])
+            {
+                string strFullPathToMaster = strDataDir + "\\Fallout4.esm";
+                File.SetCreationTime(strFullPathToMaster, datetimecreatedFallout4);
+                File.SetLastWriteTime(strFullPathToMaster, datetimecreatedFallout4);
+            }
+
+            if (boolarrayBethesdaMasters[1])
+            {
+                string strFullPathToMaster = strDataDir + "\\DLCRobot.esm";
+                File.SetCreationTime(strFullPathToMaster, datetimecreatedAutomatron);
+                File.SetLastWriteTime(strFullPathToMaster, datetimecreatedAutomatron);
+            }
+
+            if (boolarrayBethesdaMasters[2])
+            {
+                string strFullPathToMaster = strDataDir + "\\DLCworkshop01.esm";
+                File.SetCreationTime(strFullPathToMaster, datetimecreatedWastelandWorkshop);
+                File.SetLastWriteTime(strFullPathToMaster, datetimecreatedWastelandWorkshop);
+            }
+
+            if (boolarrayBethesdaMasters[3])
+            {
+                string strFullPathToMaster = strDataDir + "\\DLCCoast.esm";
+                File.SetCreationTime(strFullPathToMaster, datetimecreatedFarHarbor);
+                File.SetLastWriteTime(strFullPathToMaster, datetimecreatedFarHarbor);
+            }
+
+            if (boolarrayBethesdaMasters[4])
+            {
+                string strFullPathToMaster = strDataDir + "\\DLCworkshop02.esm";
+                File.SetCreationTime(strFullPathToMaster, datetimecreatedContraptionsWorkshop);
+                File.SetLastWriteTime(strFullPathToMaster, datetimecreatedContraptionsWorkshop);
+            }
+
+            if (boolarrayBethesdaMasters[5])
+            {
+                string strFullPathToMaster = strDataDir + "\\DLCworkshop03.esm";
+                File.SetCreationTime(strFullPathToMaster, datetimecreatedVaultTecWorkshop);
+                File.SetLastWriteTime(strFullPathToMaster, datetimecreatedVaultTecWorkshop);
+            }
+
+            if (boolarrayBethesdaMasters[6])
+            {
+                string strFullPathToMaster = strDataDir + "\\DLCNukaWorld.esm";
+                File.SetCreationTime(strFullPathToMaster, datetimecreatedNukaWorld);
+                File.SetLastWriteTime(strFullPathToMaster, datetimecreatedNukaWorld);
+            }
+        }
+
+        private void buttonRedate_Click(object sender, EventArgs e)
+        {
+            string strDataDir = textboxGetDataDir.Text;
+            MessageBox.Show(strDataDir);
+
+            bool[] boolarrayBethesdaMasterExistence;
+
+            if (strDataDir.Length > 0
+                && CheckIfArrayIsNull(strDataDir, out boolarrayBethesdaMasterExistence)
+                )
+            {
+                RedateMasters(strDataDir, boolarrayBethesdaMasterExistence);
+                MessageBox.Show("Aaaaaand we're done!");
+            }
+
+            else
+            {
+                MessageBox.Show("Nae masters found, lad. Make sure you got the proper folder, containing (any of) the official Bethesda .esm files.");
+            }
+             
+        }
+
+    }
+}
